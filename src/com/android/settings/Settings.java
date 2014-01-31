f0f9b1ac717455ef1f9000657aa30de97321c3aa
Dragon <Dragon7780@hotmail.nl>
2014-01-31 04:12:07 +0100

    Omnirom PerformanceControl
    Signed-off-by: Dragon <Dragon7780@hotmail.nl>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1217,4 +1217,5 @@ public class Settings extends PreferenceActivity
     public static class ApnEditorActivity extends Settings { /* empty */ }
     public static class BlacklistSettingsActivity extends Settings { /* empty */ }
     public static class QuietHoursSettingsActivity extends Settings { /* empty */ }
+    public static class PerformanceSettingsActivity extends Settings { /* empty */ }
 }
